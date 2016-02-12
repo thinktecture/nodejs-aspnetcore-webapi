fa87ae44a0aae15568954fe7205c85661a6b113f
Manuel Rauber <manuel.rauber@thinktecture.com>
2016-02-13 00:37:01 +0100

    Ensure DB and table creation

--- src/vnext/Services/DatabaseCustomerService.cs
+++ src/vnext/Services/DatabaseCustomerService.cs
@@ -12,6 +12,10 @@ namespace CustomerWebApi
         public DatabaseCustomerService(CustomerContext context)
         {
             _context = context;
+            
+            // Should only be used for testing and dev purposes!
+            // https://github.com/aspnet/EntityFramework/issues/3160#issuecomment-144471211
+            _context.Database.EnsureCreated();
         }
 
         public void Create(CustomerModel model)
@@ -34,7 +38,7 @@ namespace CustomerWebApi
         }
 
         public IEnumerable<CustomerModel> List()
-        {
+        {        
             return _context.Customers.ToList();
         }
     }

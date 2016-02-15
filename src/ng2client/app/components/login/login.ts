6b88f48952573881446ebad5458dd271314686cd
Manuel Rauber <manuel.rauber@thinktecture.com>
2016-02-15 23:00:19 +0100

    #5: Let's try to use native View Encapsulation everywhere...

--- src/ng2client/app/components/login/login.ts
+++ src/ng2client/app/components/login/login.ts
@@ -1,10 +1,11 @@
-import {Component} from 'angular2/core';
+import {Component, ViewEncapsulation} from 'angular2/core';
 import {STYLE_URL_FRAMEWORKS} from '../styleConfig';
 
 @Component({
     selector: 'nfn-login',
     templateUrl: 'app/components/login/login.html',
-    styleUrls: STYLE_URL_FRAMEWORKS.concat(['app/components/login/login.css'])
+    styleUrls: STYLE_URL_FRAMEWORKS.concat(['app/components/login/login.css']),
+    encapsulation: ViewEncapsulation.Native
 })
 export class LoginComponent {
 

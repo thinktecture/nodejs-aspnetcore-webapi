9f81db34f438fce4cc6ba16bd651026c2ca1e5b0
Manuel Rauber <manuel.rauber@thinktecture.com>
2016-02-17 21:14:58 +0100

    Removed web apis for customers.

--- src/nodejs/WebAPI/controllers/customer.js
+++ src/nodejs/WebAPI/controllers/customer.js
@@ -8,37 +8,7 @@ const services = require('../service');
  */
 function CustomerController() {
     this.initialize = function (server) {
-        server.get('api/customer/list', handleCustomerList);
-        server.post('api/customer', handleCustomerCreation);
-        server.del('api/customer/:id', handleCustomerDeletion);
     };
-
-    function handleCustomerList(req, res) {
-        services.get()
-            .then(srv => srv.customer.list())
-            .then(
-                customers => res.json(200, customers),
-                err => res.json(500, err)
-            );
-    }
-
-    function handleCustomerCreation(req, res) {
-        services.get()
-            .then(srv => srv.customer.create(req.body.firstName, req.body.lastName))
-            .then(
-                () => res.send(200),
-                err => res.json(500, err)
-            );
-    }
-
-    function handleCustomerDeletion(req, res) {
-        services.get()
-            .then(srv => srv.customer.remove(req.params.id))
-            .then(
-                () => res.send(200),
-                err => res.send(500, err)
-            );
-    }
 }
 
 module.exports = new CustomerController();

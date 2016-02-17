85787221752bec9b47a4fa8a32099acfe8c9f6b5
Manuel Rauber <manuel.rauber@thinktecture.com>
2016-02-17 21:15:24 +0100

    Removed sequelize model

--- src/nodejs/WebAPI/database/customerModel.js
+++ src/nodejs/WebAPI/database/customerModel.js
@@ -1,19 +1,5 @@
 'use strict';
 
 module.exports = function (sequelize, DataTypes) {
-    return sequelize.define('customer', {
-        id: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true
-        },
-        firstName: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        lastName: {
-            type: DataTypes.STRING,
-            allowNull: false
-        }
-    });
+    
 };
